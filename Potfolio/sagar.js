e13b05dbe6e29e79bb732985e9fd6b836c457d0e
Sagar Nayak <96675679+sagar9859@users.noreply.github.com>
2024-09-24 13:57:12 +0530

    Add files via upload

---
+++ Potfolio/sagar.js
@@ -0,0 +1,10 @@
+
+var typed = new Typed(".text", {
+    strings: ["Frontend Developer", "Fresher", "Web Developer","Python Developer","Software Testing"],
+    typeSpeed: 80,
+    backSpeed: 80,
+    backDelay: 800,
+    loop: true
+});
+ 
+
